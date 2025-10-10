acb61ce8b9ce0bdbfe698aa8d56a7b594eeb0460
21-george <georgi.sv.stefanov@gmail.com>
2025-10-10 17:33:19 +0300

    auto_purge.sh v1.0

---
+++ auto_purge.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Author: George S (gstefanov)
+# Date Created: 10/10/2025
+# Date Modified: 
+# Version: 1.0
+
+# Description:
+# Automates the purge of multiple buckets
+
+# Usage
+# ./auto_purge.sh <file>
+
+# Variables
+BUCKET_LIST=$1
+PURGE_HOME=/root/purge
+PURGE=/root/cloudian-bucket-tools/bin/cloudian-bucket-purge
+PART_LOG=${BUCKET}.purged.partitions.1.log
+
+# Checks
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <file>"
+  echo "<file> should contain one bucket per line."
+  exit 1
+fi
+
+if [ ! -f "$BUCKET_LIST" ]; then
+  echo "Error: File $BUCKET_LIST does not exist."
+  echo "Usage: $0 <file>"
+  exit 1
+fi
+
+if [ ! -s "$BUCKET_LIST" ]; then
+  echo "Error: File $BUCKET_LIST is empty."
+  exit 1
+fi
+
+# Functions
+purge_api () {
+    curl -X POST -k -u $(hsctl config get admin.auth.username):$(hsctl config get admin.auth.password) https://localhost:19443/bucketops/purge?bucketName="$BUCKET"
+}
+
+# Run
+mkdir $PURGE_HOME && cd $PURGE_HOME
+
+while IFS= read -r BUCKET; do
+    purge_api
+    $PURGE -b "$BUCKET"
+    $PURGE -b "$BUCKET" -file $PART_LOG -t 15 -dry n
+done < "$BUCKET_LIST"
