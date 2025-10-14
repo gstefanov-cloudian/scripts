c0bebbd01fd85d94d1e76a548f2ec62dc1cfae14
21-george <georgi.sv.stefanov@gmail.com>
2025-10-14 15:48:08 +0300

    auto_purge.sh v1.1
    - Added functionality to the purge_api function allowing it to be executed on all nodes on the system, not only SaltMaster.

--- auto_purge.sh
+++ auto_purge.sh
@@ -2,7 +2,7 @@
 # Author: George S (gstefanov)
 # Date Created: 10/10/2025
 # Date Modified: 
-# Version: 1.0
+# Version: 1.1
 
 # Description:
 # Automates the purge of multiple buckets
@@ -16,6 +16,7 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 BUCKET_LIST=$1
 PURGE_HOME=/root/purge
 PURGE=/root/cloudian-bucket-tools/bin/cloudian-bucket-purge
+MASTER=$(grep -A5 installscript /opt/cloudian/conf/cloudianservicemap.json | grep hostname | awk -F'"' '{print$4}')
 
 # Checks
 if [ $# -ne 1 ]; then
@@ -37,16 +38,24 @@ fi
 
 # Functions
 purge_api () {
-    curl -X POST -k -u $(hsctl config get admin.auth.username):$(hsctl config get admin.auth.password) https://localhost:19443/bucketops/purge?bucketName="$BUCKET"
+    local USER PASS
+    if [[ "${MASTER}" != "${HOSTNAME}" ]] ; then
+      USER=$(ssh -i /export/home/cloudian/cloudian-installation-key "${MASTER}" "hsctl config get admin.auth.username")
+      PASS=$(ssh -i /export/home/cloudian/cloudian-installation-key "${MASTER}" "hsctl config get admin.auth.password")
+    else
+      USER=$(hsctl config get admin.auth.username)
+      PASS=$(hsctl config get admin.auth.password)
+    fi
+    curl -X POST -k -u "${USER}:${PASS}" https://localhost:19443/bucketops/purge?bucketName="${BUCKET}" </dev/null
 }
 
 # Run
 mkdir -p "$PURGE_HOME"
-cd "$PURGE_HOME"
+cd "$PURGE_HOME" || exit
 
-while IFS= read -r BUCKET; do
+while read -r BUCKET; do
     [[ -z "$BUCKET" ]] && continue  # skip empty lines
     purge_api
-    $PURGE -b "$BUCKET"
-    $PURGE -b "$BUCKET" -file ${BUCKET}.purged.partitions.1.log -t 15 -dry n
+    $PURGE -b "$BUCKET" </dev/null
+    $PURGE -b "$BUCKET" -file "${BUCKET}.purged.partitions.1.log" -t 15 -dry n </dev/null
 done < "${DIR}/${BUCKET_LIST}"
